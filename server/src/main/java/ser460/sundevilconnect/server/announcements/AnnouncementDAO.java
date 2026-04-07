4ef6e5c303b022868c82cfb3b2926bfbebf8aab3
hdathert <hdathert@asu.edu>
2026-04-07 16:32:09 -0700

    Creates queries for AnnouncementDAO

--- server/src/main/java/ser460/sundevilconnect/server/announcements/AnnouncementDAO.java
+++ server/src/main/java/ser460/sundevilconnect/server/announcements/AnnouncementDAO.java
@@ -7,9 +7,10 @@ import ser460.sundevilconnect.server.clubs.ClubDAO;
 import ser460.sundevilconnect.server.core.DatabaseService;
 import ser460.sundevilconnect.shared.proto.EntitiesProto;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnnouncementDAO {
     private final DatabaseService db;
@@ -18,7 +19,90 @@ public class AnnouncementDAO {
         this.db = db;
     }
 
+    public List<Announcement> getAnnouncementsForClub(int clubId, boolean includeDrafts) throws SQLException {
+        List<Announcement> announcements = new ArrayList<>();
+        String sql = """
+                SELECT a.*,
+                    u.id as userId, u.firstName, u.lastName,
+                    c.clubId, c.name as clubName
+                FROM announcements a
+                JOIN users u ON a.createdBy = u.id
+                JOIN clubs c ON a.postedToClub = c.clubId
+                WHERE a.postedToClub = ? AND a.status != 'REMOVED'
+                """ + (includeDrafts ? "" : "AND a.status = 'PUBLISHED'") +
+                " ORDER BY a.postedDate DESC";
 
+        try (Connection conn = db.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, clubId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                announcements.add(mapRow(rs));
+            }
+        }
+        return announcements;
+    }
+
+    public String createAnnouncement(int clubId, int createdBy, String title, String body, String status) throws SQLException {
+        String sql = "INSERT INTO announcements (title, body, postedToClub, createdBy, status, postedDate) " +
+                "VALUES  (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = db.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+            ps.setString(1, title);
+            ps.setString(2, body);
+            ps.setInt(3, clubId);
+            ps.setInt(4, createdBy);
+            ps.setString(5, status);
+            ps.setString(6, "PUBLISHED".equals(status)
+                    ? LocalDate.now().toString()
+                    : null);
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                return String.valueOf(rs.getInt(1));
+            }
+        }
+        throw new SQLException("Failed to create announcement");
+    }
+
+    public boolean updateAnnouncement(int announcementId, String title, String body, String status) throws SQLException {
+        String sql = "UPDATE announcements SET title = ?, body = ?, status = ?, postedDate = ? WHERE id = ?";
+        try (Connection conn = db.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setString(1, title);
+            ps.setString(2, body);
+            ps.setString(3, status);
+            ps.setString(4, "PUBLISHED".equals(status)
+                    ? LocalDate.now().toString()
+                    : null);
+            ps.setInt(5, announcementId);
+            return ps.executeUpdate() > 0;
+        }
+    }
+
+    public boolean deleteAnnouncement(int announcementId) throws SQLException {
+        String sql = "UPDATE announcements SET status = 'REMOVED' WHERE id = ?";
+        try (Connection conn = db.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);) {
+            ps.setInt(1, announcementId);
+            return ps.executeUpdate() > 0;
+        }
+    }
+
+    public boolean publish(int announcementId, boolean isPublished) throws SQLException {
+        String sql = isPublished
+                ? "UPDATE announcements SET status = 'PUBLISHED', postedDate = ? WHERE id = ?"
+                : "UPDATE announcements SET status = 'DRAFT', postedDate = NULL WHERE id = ?";
+        try (Connection conn = db.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            if (isPublished) {
+                ps.setString(1, LocalDate.now().toString());
+                ps.setInt(2, announcementId);
+            } else {
+                ps.setInt(1, announcementId);
+            }
+            return ps.executeUpdate() > 0;
+        }
+    }
 
     private Announcement mapRow(ResultSet rs) throws SQLException {
         Student createdBy = new Student(
