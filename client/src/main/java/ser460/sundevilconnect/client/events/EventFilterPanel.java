2c0230797104b1924626f860628b7171935e08b1
Weronika Golden <wgolden117@gmail.com>
2026-04-09 18:02:41 -0700

    Unify Events and My Events layouts with reusable UI and cancel functionality

--- client/src/main/java/ser460/sundevilconnect/client/events/EventFilterPanel.java
+++ client/src/main/java/ser460/sundevilconnect/client/events/EventFilterPanel.java
@@ -1,12 +1,34 @@
 package ser460.sundevilconnect.client.events;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 
 public class EventFilterPanel {
-    // TODO: define @FXML fields once UI design is determined
+
+    @FXML private ComboBox<String> categoryDropdown;
+    @FXML private CheckBox paidCheckbox;
 
     @FXML
     private void initialize() {
-        // TODO: called by JavaFX after FXML loads, fetch initial data from server
+        // Populate dropdown (simple static list for now)
+        categoryDropdown.getItems().addAll(
+                "Technology",
+                "Music",
+                "Business",
+                "Art",
+                "Health",
+                "Recreation"
+        );
+    }
+
+    @FXML
+    private void handleApplyFilters() {
+        String selectedCategory = categoryDropdown.getValue();
+        boolean isPaid = paidCheckbox.isSelected();
+
+        System.out.println("Filters selected:");
+        System.out.println("Category: " + selectedCategory);
+        System.out.println("Paid: " + isPaid);
     }
 }
