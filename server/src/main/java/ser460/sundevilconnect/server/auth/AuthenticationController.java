e6c4f3db3e48b9c805bd1288a5b6f38b45d1e3b3
hdathert <hdathert@asu.edu>
2026-04-15 13:32:26 -0700

    Adds authentication logging in console for debug purposes

--- server/src/main/java/ser460/sundevilconnect/server/auth/AuthenticationController.java
+++ server/src/main/java/ser460/sundevilconnect/server/auth/AuthenticationController.java
@@ -2,6 +2,7 @@ package ser460.sundevilconnect.server.auth;
 
 import io.grpc.stub.StreamObserver;
 import ser460.sundevilconnect.server.core.AuthenticationService;
+import ser460.sundevilconnect.server.core.NotificationService;
 import ser460.sundevilconnect.shared.proto.AuthServiceGrpc.*;
 import ser460.sundevilconnect.shared.proto.AuthServiceProto.*;
 import ser460.sundevilconnect.shared.proto.EntitiesProto.*;
@@ -21,17 +22,17 @@ public class AuthenticationController extends AuthServiceImplBase {
 
         if (sessionToken != null) {
             User user = authService.getSessionUser(sessionToken);
-
+            String displayName = user.getFirstName() + " " + user.getLastName();
             responseObserver.onNext(LoginResponse.newBuilder()
                     .setSuccess(true)
                     .setUser(UserSummary.newBuilder()
-                            .setDisplayName(user.getFirstName() + " " + user.getLastName())
+                            .setDisplayName(displayName)
                             .setUserId(user.getUserId())
                             .build())
                     .setToken(sessionToken)
                     .setRole(user.getRole())
                     .build());
-
+            System.out.printf(":: LOGIN :: \n\t%s\n\t%s\n", user.getUserId(), sessionToken);
         } else {
             responseObserver.onNext(LoginResponse.newBuilder()
                     .setSuccess(false)
@@ -47,17 +48,14 @@ public class AuthenticationController extends AuthServiceImplBase {
         // invalidate session
         boolean success = AuthenticationService.getInstance().logout(request.getToken());
 
-        // TODO: call NotificationService.getInstance().detachAll(userId)
+        NotificationService.getInstance().detachAll(request.getUserId());
 
         responseObserver.onNext(LogoutResponse.newBuilder()
                 .setSuccess(success)
                 .build());
-
+        System.out.printf(":: LOGOUT :: \n\t%s\n\t%s\n", request.getUserId(), request.getToken());
         responseObserver.onCompleted();
     }
-
-    public void resetPassword(String email) {}
-    private boolean validateCredentials(String username, String password) { return false; }
 
     // TODO figure out how we want to create new Students/Admins etc. We'll have to add them to the proto file
     // and then also add the service methods in here.
